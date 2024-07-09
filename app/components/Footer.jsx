94430ccd95c218fa7e1ca380bd31792878681f53
Alexander Filip Petruška <118067191+AlexanderPetruska@users.noreply.github.com>
2024-07-09 19:19:23 +0200

    Update Footer.jsx

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -3,7 +3,7 @@ import { footerLinks } from "../constants";
 
 const Footer = () => {
   return (
-    <footer className="p-5 sm:px-10 ">
+    <footer className="p-7 sm:px-10 ">
       <div className="screen-max-width">
         <div>
           <p className="text-xs font-semibold text-gray">
